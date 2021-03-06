3f2213d30fa0e8b2b0926b29d0858f02ff839382
mrkmx <alex.a.maximow@gmail.com>
2021-03-07 02:34:49 +0400

    create Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_223156) do
+ActiveRecord::Schema.define(version: 2021_03_06_223310) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 2021_03_06_223156) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["test_id"], name: "index_answers_on_test_id"
     t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
