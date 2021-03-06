f82f86401f5923fbc76424a78b7e31409d0482ff
mrkmx <alex.a.maximow@gmail.com>
2021-03-07 02:32:27 +0400

    create User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_222958) do
+ActiveRecord::Schema.define(version: 2021_03_06_223156) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -38,6 +38,13 @@ ActiveRecord::Schema.define(version: 2021_03_06_222958) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["category_id"], name: "index_tests_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.boolean "admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "answers", "tests"
