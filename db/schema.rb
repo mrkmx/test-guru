261d7111a46c97c264a3f6f5ece92a0e0fed03e3
mrkmx <alex.a.maximow@gmail.com>
2021-03-07 03:07:03 +0400

    add NOT NULL constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_223310) do
+ActiveRecord::Schema.define(version: 2021_03_06_224653) do
 
   create_table "answers", force: :cascade do |t|
-    t.string "body"
-    t.boolean "correct"
+    t.string "body", null: false
+    t.boolean "correct", null: false
     t.integer "test_id", null: false
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -24,13 +24,13 @@ ActiveRecord::Schema.define(version: 2021_03_06_223310) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.string "body"
+    t.string "body", null: false
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -38,8 +38,8 @@ ActiveRecord::Schema.define(version: 2021_03_06_223310) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
-    t.integer "level"
+    t.string "title", null: false
+    t.integer "level", null: false
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2021_03_06_223310) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.boolean "admin"
+    t.string "name", null: false
+    t.boolean "admin", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
