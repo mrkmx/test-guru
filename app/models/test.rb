c6d3a321dde94998db4fa2a787a318b78345038c
mrkmx <alex.a.maximow@gmail.com>
2021-03-09 22:54:43 +0400

    tests by category title

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,7 @@
 class Test < ApplicationRecord
   belongs_to :category
+
+  def self.by_category(title)
+    Test.includes(:category).where('categories.title = ?', title).order(title: :desc).pluck(:title)
+  end
 end
