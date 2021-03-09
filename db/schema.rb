e230923d5541be86ca0824ae33733b88f27a5bad
mrkmx <alex.a.maximow@gmail.com>
2021-03-09 21:55:44 +0400

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,10 @@ ActiveRecord::Schema.define(version: 2021_03_06_225500) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.boolean "correct", default: false, null: false
-    t.integer "test_id", null: false
-    t.integer "user_id", null: false
+    t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_answers_on_test_id"
-    t.index ["user_id"], name: "index_answers_on_user_id"
+    t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -53,8 +51,7 @@ ActiveRecord::Schema.define(version: 2021_03_06_225500) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "answers", "tests"
-  add_foreign_key "answers", "users"
+  add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
