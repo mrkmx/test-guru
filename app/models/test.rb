3b88d0c7c97983b5d05989103972e7090026a94c
mrkmx <alex.a.maximow@gmail.com>
2021-03-09 23:08:41 +0400

    Update test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.by_category(title)
-    Test.includes(:category).where('categories.title = ?', title).order(title: :desc).pluck(:title)
+    joins(:category).where('categories.title = ?', title).order(title: :desc).pluck(:title)
   end
 end
