de9cf83c37c910107d156ac71b1f7bd80b3846b9
mrkmx <alex.a.maximow@gmail.com>
2021-03-10 21:48:43 +0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,36 +10,36 @@ categories = Category.create!([
 ])
 
 tests = Test.create!([
-  {title: 'Test 1', level: 0, category_id: categories[0][:id]},
-  {title: 'Test 2', level: 1, category_id: categories[1][:id]},
-  {title: 'Test 3', level: 2, category_id: categories[1][:id]},
+  {title: 'Test 1', level: 0, category_id: categories[0].id},
+  {title: 'Test 2', level: 1, category_id: categories[1].id},
+  {title: 'Test 3', level: 2, category_id: categories[1].id},
 ])
 
 questions = Question.create!([
-  {body: 'Question 1-1', test_id: tests[0][:id]},
-  {body: 'Question 1-2', test_id: tests[0][:id]},
-  {body: 'Question 1-3', test_id: tests[0][:id]},
-  {body: 'Question 2-1', test_id: tests[1][:id]},
-  {body: 'Question 2-2', test_id: tests[1][:id]},
-  {body: 'Question 2-3', test_id: tests[1][:id]},
-  {body: 'Question 3-1', test_id: tests[2][:id]},
-  {body: 'Question 3-2', test_id: tests[2][:id]},
-  {body: 'Question 3-3', test_id: tests[2][:id]},
+  {body: 'Question 1-1', test_id: tests[0].id},
+  {body: 'Question 1-2', test_id: tests[0].id},
+  {body: 'Question 1-3', test_id: tests[0].id},
+  {body: 'Question 2-1', test_id: tests[1].id},
+  {body: 'Question 2-2', test_id: tests[1].id},
+  {body: 'Question 2-3', test_id: tests[1].id},
+  {body: 'Question 3-1', test_id: tests[2].id},
+  {body: 'Question 3-2', test_id: tests[2].id},
+  {body: 'Question 3-3', test_id: tests[2].id},
 ])
 
 answers = Answer.create!([
-  {body: 'Answer 1', question_id: questions[0][:id]},
-  {body: 'Answer 2', question_id: questions[1][:id]},
-  {body: 'Answer 3', question_id: questions[2][:id]},
-  {body: 'Answer 4', question_id: questions[3][:id]},
-  {body: 'Answer 5', question_id: questions[4][:id]},
-  {body: 'Answer 6', question_id: questions[5][:id]},
-  {body: 'Answer 7', question_id: questions[6][:id]},
-  {body: 'Answer 8', question_id: questions[7][:id]},
-  {body: 'Answer 9', question_id: questions[8][:id]},
+  {body: 'Answer 1', question_id: questions[0].id},
+  {body: 'Answer 2', question_id: questions[1].id},
+  {body: 'Answer 3', question_id: questions[2].id},
+  {body: 'Answer 4', question_id: questions[3].id},
+  {body: 'Answer 5', question_id: questions[4].id},
+  {body: 'Answer 6', question_id: questions[5].id},
+  {body: 'Answer 7', question_id: questions[6].id},
+  {body: 'Answer 8', question_id: questions[7].id},
+  {body: 'Answer 9', question_id: questions[8].id},
 ])
 
 results = Result.create!([
-  {user_id: users[0][:id], test_id: tests[0][:id]},
-  {user_id: users[0][:id], test_id: tests[1][:id]},
+  {user_id: users[0].id, test_id: tests[0].id},
+  {user_id: users[0].id, test_id: tests[1].id},
 ])
