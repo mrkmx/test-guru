93d7cf9f72e3717c9499433d7fab0bade58b19e6
mrkmx <alex.a.maximow@gmail.com>
2021-03-11 21:12:32 +0400

    del tests_users unique

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 2021_03_10_193127) do
   create_table "tests_users", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "test_id", null: false
-    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id", unique: true
+    t.index ["user_id", "test_id"], name: "index_tests_users_on_user_id_and_test_id"
   end
 
   create_table "users", force: :cascade do |t|
