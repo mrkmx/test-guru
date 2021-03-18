a45849a1c2664a995a5a9d869c38e5c153332e60
mrkmx <alex.a.maximow@gmail.com>
2021-03-18 21:57:15 +0400

    task 1

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,7 +5,18 @@ class Test < ApplicationRecord
   has_many :tests_users
   has_many :users, through: :tests_users
 
+  scope :by_level, -> (level) { where(level: level_range(level)) }
+
   def self.by_category(title)
     joins(:category).where('categories.title = ?', title).order(title: :desc).pluck(:title)
   end
+
+  private
+
+  def self.level_range(level)
+    return 0..1 if level == :easy
+    return 2..4 if level == :medium
+    return 5..Float::INFINITY if level == :hard
+  end
+
 end
