004a51008e735459df4d04c122431c7dfdf618c9
mrkmx <alex.a.maximow@gmail.com>
2021-03-18 23:13:36 +0400

    make validate method private

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,6 +6,8 @@ class Answer < ApplicationRecord
 
   scope :correct, -> { where(correct: true) }
 
+  private
+
   def validate_answers_count
     errors.add(:question, 'У одного Вопроса может быть от 1-го до 4-х ответов.') if question.answers.count >= 4
   end
