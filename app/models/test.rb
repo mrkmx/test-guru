0082f64724cf1ec047f80fe2bf85d98e3f5265e4
mrkmx <alex.a.maximow@gmail.com>
2021-06-12 14:19:53 +0400

    del test_passage earlier than question

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
-  has_many :questions, dependent: :destroy
   has_many :test_passages, dependent: :destroy
+  has_many :questions, dependent: :destroy
   has_many :users, through: :test_passages
 
   validates :title, presence: true,
