d76dc5a38c92458f18a893a2b86e5a0d77493aef
mrkmx <alex.a.maximow@gmail.com>
2021-06-17 00:24:29 +0400

    fix git desktop client bug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_190442) do
 ActiveRecord::Schema.define(version: 2021_06_16_183602) do
 
   # These are extensions that must be enabled in order to support this database
